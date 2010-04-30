b2e557344e9e081d3ad72b06f79775d798cd0fb7
desandro <david@desandro.com>
2010-04-30 08:04:54 -0400

    v1.1.0 no longer beta

--- js/jquery.masonry.js
+++ js/jquery.masonry.js
@@ -1,5 +1,5 @@
 /*************************************************
-**  jQuery Masonry version 1.1.0 beta
+**  jQuery Masonry version 1.1.0
 **  copyright David DeSandro, licensed GPL & MIT
 **  http://desandro.com/resources/jquery-masonry
 **************************************************/
