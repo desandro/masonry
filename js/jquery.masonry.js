ec7d8257d967c733a9322210c560d1e3b5bae510
desandro <david@desandro.com>
2010-06-19 21:38:54 -0400

    place all interior functions under msnry. methods

--- js/jquery.masonry.js
+++ js/jquery.masonry.js
@@ -49,156 +49,155 @@
     // masonry code begin
     $.fn.masonry = function(options, callback) { 
 
-        function getBricks(props, opts) {
-            props.$bricks = opts.itemSelector == undefined ?
-                        opts.$brickParent.children() :
-                        opts.$brickParent.find(opts.itemSelector);
-        }
-
-        function placeBrick($brick, setCount, setY, setSpan, props, opts) {
-            var shortCol = 0;
+        // all my sweet methods
+        var msnry = {
+            getBricks : function(props, opts) {
+                props.$bricks = opts.itemSelector == undefined ?
+                            opts.$brickParent.children() :
+                            opts.$brickParent.find(opts.itemSelector);
+            },
             
-            for ( i=0; i < setCount; i++ ) {
-                if ( setY[i] < setY[ shortCol ] ) shortCol = i;
-            }
-            
-            var position = {
-                left: props.colW * shortCol + props.posLeft,
-                top: setY[ shortCol ]
-            };
+            placeBrick : function($brick, setCount, setY, setSpan, props, opts) {
+                var shortCol = 0;
 
-            $brick.applyStyle(position, $.extend(true,{},opts.animationOptions) );
+                for ( i=0; i < setCount; i++ ) {
+                    if ( setY[i] < setY[ shortCol ] ) shortCol = i;
+                }
 
-            for ( i=0; i < setSpan; i++ ) {
-                props.colY[ shortCol + i ] = setY[ shortCol ] + $brick.outerHeight(true) ;
-            }
-        };
-
-
-        function masonrySetup($wall, opts, props) {
-             getBricks(props, opts);
-
-            if ( opts.columnWidth == undefined) {
-                props.colW = props.masoned ?
-                        $wall.data('masonry').colW :
-                        props.$bricks.outerWidth(true);
-            } else {
-                props.colW = opts.columnWidth;
-            }
-
-            props.colCount = Math.floor( $wall.width() / props.colW ) ;
-            props.colCount = Math.max( props.colCount, 1 );
-        };
-
-
-        function masonryArrange($wall, opts, props) {
-            // if masonry hasn't been called before
-            if( !props.masoned ) $wall.css( 'position', 'relative' );            
-            
-            if ( !props.masoned || opts.appendedContent != undefined ) {
-                // just the new bricks
-                props.$bricks.css( 'position', 'absolute' );
-            }
-
-            // get top left position of where the bricks should be
-            var cursor = $('<div />');
-            $wall.prepend( cursor );
-            props.posTop =  Math.round( cursor.position().top );
-            props.posLeft = Math.round( cursor.position().left );
-            cursor.remove();
-
-            // set up column Y array
-            if ( props.masoned && opts.appendedContent != undefined ) {
-                // if appendedContent is set, use colY from last call
-                props.colY = $wall.data('masonry').colY;
-                
-                /*
-                *  in the case that the wall is not resizeable,
-                *  but the colCount has changed from the previous time
-                *  masonry has been called
-                */
-                for (i= $wall.data('masonry').colCount; i < props.colCount; i++) {
-                    props.colY[i] = props.posTop;
+                var position = {
+                    left: props.colW * shortCol + props.posLeft,
+                    top: setY[ shortCol ]
                 };
-                
-            } else {
-                props.colY = [];
-                for ( i=0; i < props.colCount; i++) {
-                    props.colY[i] = props.posTop;
-                }    
-            }
-            
-            // are we animating the rearrangement?
-            // use plugin-ish syntax for css or animate
-            $.fn.applyStyle = ( props.masoned && opts.animate ) ? $.fn.animate : $.fn.css;
-            
-            
-            // layout logic
-            if ( opts.singleMode ) {
-                props.$bricks.each(function(){
-                    var $brick = $(this);
-                    placeBrick($brick, props.colCount, props.colY, 1, props, opts);
-                });            
-            } else {
-                props.$bricks.each(function() {
-                    var $brick = $(this);
-                
-                    //how many columns does this brick span
-                    var colSpan = Math.ceil( $brick.outerWidth(true) / props.colW);
-                    colSpan = Math.min( colSpan, props.colCount );
 
-                    
-                    if ( colSpan == 1 ) {
-                        // if brick spans only one column, just like singleMode
-                        placeBrick($brick, props.colCount, props.colY, 1, props, opts);
-                    } else {
-                        // brick spans more than one column
+                $brick.applyStyle(position, $.extend(true,{},opts.animationOptions) );
 
-                        //how many different places could this brick fit horizontally
-                        var groupCount = props.colCount + 1 - colSpan; 
-                        var groupY = [0];
-                        // for each group potential horizontal position
-                        for ( i=0; i < groupCount; i++ ) {
-                            groupY[i] = 0;
-                            // for each column in that group
-                            for ( j=0; j < colSpan; j++ ) {
-                                // get the maximum column height in that group
-                                groupY[i] = Math.max( groupY[i], props.colY[i+j] );
+                for ( i=0; i < setSpan; i++ ) {
+                    props.colY[ shortCol + i ] = setY[ shortCol ] + $brick.outerHeight(true) ;
+                }
+            },
+            
+            setup : function($wall, opts, props) {
+                 msnry.getBricks(props, opts);
+
+                if ( opts.columnWidth == undefined) {
+                    props.colW = props.masoned ?
+                            $wall.data('masonry').colW :
+                            props.$bricks.outerWidth(true);
+                } else {
+                    props.colW = opts.columnWidth;
+                }
+
+                props.colCount = Math.floor( $wall.width() / props.colW ) ;
+                props.colCount = Math.max( props.colCount, 1 );
+            },
+            
+            arrange : function($wall, opts, props) {
+                // if masonry hasn't been called before
+                if( !props.masoned ) $wall.css( 'position', 'relative' );            
+
+                if ( !props.masoned || opts.appendedContent != undefined ) {
+                    // just the new bricks
+                    props.$bricks.css( 'position', 'absolute' );
+                }
+
+                // get top left position of where the bricks should be
+                var cursor = $('<div />');
+                $wall.prepend( cursor );
+                props.posTop =  Math.round( cursor.position().top );
+                props.posLeft = Math.round( cursor.position().left );
+                cursor.remove();
+
+                // set up column Y array
+                if ( props.masoned && opts.appendedContent != undefined ) {
+                    // if appendedContent is set, use colY from last call
+                    props.colY = $wall.data('masonry').colY;
+
+                    /*
+                    *  in the case that the wall is not resizeable,
+                    *  but the colCount has changed from the previous time
+                    *  masonry has been called
+                    */
+                    for (i= $wall.data('masonry').colCount; i < props.colCount; i++) {
+                        props.colY[i] = props.posTop;
+                    };
+
+                } else {
+                    props.colY = [];
+                    for ( i=0; i < props.colCount; i++) {
+                        props.colY[i] = props.posTop;
+                    }    
+                }
+
+                // are we animating the rearrangement?
+                // use plugin-ish syntax for css or animate
+                $.fn.applyStyle = ( props.masoned && opts.animate ) ? $.fn.animate : $.fn.css;
+
+
+                // layout logic
+                if ( opts.singleMode ) {
+                    props.$bricks.each(function(){
+                        var $brick = $(this);
+                        msnry.placeBrick($brick, props.colCount, props.colY, 1, props, opts);
+                    });            
+                } else {
+                    props.$bricks.each(function() {
+                        var $brick = $(this);
+
+                        //how many columns does this brick span
+                        var colSpan = Math.ceil( $brick.outerWidth(true) / props.colW);
+                        colSpan = Math.min( colSpan, props.colCount );
+
+
+                        if ( colSpan == 1 ) {
+                            // if brick spans only one column, just like singleMode
+                            msnry.placeBrick($brick, props.colCount, props.colY, 1, props, opts);
+                        } else {
+                            // brick spans more than one column
+
+                            //how many different places could this brick fit horizontally
+                            var groupCount = props.colCount + 1 - colSpan; 
+                            var groupY = [0];
+                            // for each group potential horizontal position
+                            for ( i=0; i < groupCount; i++ ) {
+                                groupY[i] = 0;
+                                // for each column in that group
+                                for ( j=0; j < colSpan; j++ ) {
+                                    // get the maximum column height in that group
+                                    groupY[i] = Math.max( groupY[i], props.colY[i+j] );
+                                }
                             }
+
+                            msnry.placeBrick($brick, groupCount, groupY, colSpan, props, opts);
                         }
+                    }); //        /props.bricks.each(function() {
+                }  //         /layout logic
+
+                // set the height of the wall to the tallest column
+                props.wallH = 0;
+                for ( i=0; i < props.colCount; i++ ) {
+                    props.wallH = Math.max( props.wallH, props.colY[i] );
+                }
+                var wallCSS = { height: props.wallH - props.posTop };
+                $wall.applyStyle( wallCSS, $.extend(true,[],opts.animationOptions) )
+
+                // add masoned class first time around
+                if ( !props.masoned ) $wall.addClass('masoned');
+
+                // provide props.bricks as context for the callback
+                callback.call( props.$bricks );
+
+                // set all data so we can retrieve it for appended appendedContent
+                //        or anyone else's crazy jquery fun
+                $wall.data('masonry', props );
                 
-                        placeBrick($brick, groupCount, groupY, colSpan, props, opts);
-                    }
-                }); //        /props.bricks.each(function() {
-            }  //         /layout logic
-        
-            // set the height of the wall to the tallest column
-            props.wallH = 0;
-            for ( i=0; i < props.colCount; i++ ) {
-                props.wallH = Math.max( props.wallH, props.colY[i] );
-            }
-            var wallCSS = { height: props.wallH - props.posTop };
-            $wall.applyStyle( wallCSS, $.extend(true,[],opts.animationOptions) )
-
-            // add masoned class first time around
-            if ( !props.masoned ) $wall.addClass('masoned');
-
-            // provide props.bricks as context for the callback
-            callback.call( props.$bricks );
+            }, // /msnry.arrange
             
-            // set all data so we can retrieve it for appended appendedContent
-            //        or anyone else's crazy jquery fun
-            $wall.data('masonry', props );
-
-
-        }; //  /masonryArrange function
-
-
-        function masonryResize($wall, opts, props) {
-            props.masoned = $wall.data('masonry') != undefined;
-            var prevColCount = $wall.data('masonry').colCount;
-            masonrySetup($wall, opts, props);
-            if ( props.colCount != prevColCount ) masonryArrange($wall, opts, props); 
+            resize : function($wall, opts, props) {
+                props.masoned = $wall.data('masonry') != undefined;
+                var prevColCount = $wall.data('masonry').colCount;
+                msnry.setup($wall, opts, props);
+                if ( props.colCount != prevColCount ) msnry.arrange($wall, opts, props); 
+            }
         };
 
 
@@ -238,18 +237,18 @@
                 opts.$brickParent = $wall;
             }
             
-            getBricks(props, opts);
+            msnry.getBricks(props, opts);
 
 
             if ( props.$bricks.length ) {
                 // call masonry layout
-                masonrySetup($wall, opts, props);
-                masonryArrange($wall, opts, props);
+                msnry.setup($wall, opts, props);
+                msnry.arrange($wall, opts, props);
             
                 // binding window resizing
                 var resizeOn = previousOptions.resizeable;
                 if ( !resizeOn && opts.resizeable ) {
-                    $(window).bind('smartresize.masonry', function() { masonryResize($wall, opts, props); } );
+                    $(window).bind('smartresize.masonry', function() { msnry.resize($wall, opts, props); } );
                 }
                 if ( resizeOn && !opts.resizeable ) $(window).unbind('smartresize.masonry');
             } else {
