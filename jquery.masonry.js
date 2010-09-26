e3fb9cf8a75bbdd32b487cca414d257aec652ace
desandro <david@desandro.com>
2010-09-25 22:39:11 -0400

    version # 1.3.2 change docs to use masonry.min.js

--- jquery.masonry.js
+++ jquery.masonry.js
@@ -1,5 +1,5 @@
 /*************************************************
-**  jQuery Masonry version 1.3.1
+**  jQuery Masonry version 1.3.2
 **  Copyright David DeSandro, licensed MIT
 **  http://desandro.com/resources/jquery-masonry
 **************************************************/
