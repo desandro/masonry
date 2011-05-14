31ae0b9ec294c638542a49bda7647eb62dcc39cd
David DeSandro <david@desandro.com>
2011-05-14 12:01:13 -0400

    src : add imagesLoaded plugin

--- jquery.masonry.js
+++ jquery.masonry.js
@@ -294,5 +294,38 @@
     animate: false,
     animationOptions: {}
   };
+  
+  // ======================= imagesLoaded Plugin  ===============================
+  // A fork of http://gist.github.com/268257 by Paul Irish
+
+  // mit license. paul irish. 2010.
+  // webkit fix from Oren Solomianik. thx!
+
+  $.fn.imagesLoaded = function(callback){
+    var elems = this.find('img'),
+        len   = elems.length,
+        _this = this;
+
+    if ( !elems.length ) {
+      callback.call( this );
+    }
+
+    elems.bind('load',function(){
+      if (--len <= 0){ 
+        callback.call( _this ); 
+      }
+    }).each(function(){
+      // cached images don't fire load sometimes, so we reset src.
+      if (this.complete || this.complete === undefined){
+        var src = this.src;
+        // webkit hack from http://groups.google.com/group/jquery-dev/browse_thread/thread/eee6ab7b2da50e1f
+        // data uri bypasses webkit log warning (thx doug jones)
+        this.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+        this.src = src;
+      }  
+    }); 
+
+    return this;
+  };
 
 })(jQuery);
