07a349867efb698302be6623e2f67bba0ae424b5
David DeSandro <desandrocodes@gmail.com>
2013-02-08 13:02:21 -0500

    js : use $.event.dispatch
    .handle was making jQuery Migrate whiny
    
    tick version

--- jquery.masonry.js
+++ jquery.masonry.js
@@ -1,5 +1,5 @@
 /**
- * jQuery Masonry v2.1.07
+ * jQuery Masonry v2.1.08
  * A dynamic layout plugin for jQuery
  * The flip-side of CSS Floats
  * http://masonry.desandro.com
@@ -26,7 +26,6 @@
    */
 
   var $event = $.event,
-      dispatchMethod = $.event.handle ? 'handle' : 'dispatch',
       resizeTimeout;
 
   $event.special.smartresize = {
@@ -46,7 +45,7 @@
 
       if ( resizeTimeout ) { clearTimeout( resizeTimeout ); }
       resizeTimeout = setTimeout(function() {
-        $event[ dispatchMethod ].apply( context, args );
+        $event.dispatch.apply( context, args );
 
       }, execAsap === "execAsap"? 0 : 100 );
     }
