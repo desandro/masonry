cd4386fabb0dac1b520eec156ba07378d3f78a3e
David DeSandro <david@desandro.com>
2013-05-03 23:30:56 -0400

    add masonry.js and .jshintrc

---
+++ masonry.js
@@ -0,0 +1,11 @@
+/*!
+ * Masonry
+ * grid layout, like a mason fitting stones in a wall
+ * http://masonry.desandro.com
+ * MIT License
+ * by David DeSandro
+ */
+
+( function( window ) {
+
+})( window );
