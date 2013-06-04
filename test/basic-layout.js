703aa84a79113b96495270b6a7ad40676d18eefb
David DeSandro <desandrocodes@gmail.com>
2013-06-04 18:23:03 -0400

    add qunit test

---
+++ test/basic-layout.js
@@ -0,0 +1,174 @@
+( function() {
+
+'use strict';
+
+test( 'basic layout top left', function() {
+  var container = document.querySelector('#basic-layout-top-left');
+  var msnry = new Masonry( container, {
+    columnWidth: 60
+  });
+
+  var positions = {
+    0: {
+      left: 0,
+      top: 0
+    },
+    1: {
+      left: 60,
+      top: 0
+    },
+    2: {
+      left: 120,
+      top: 0
+    },
+    3: {
+      left: 0,
+      top: 30
+    },
+    4: {
+      left: 60,
+      top: 90
+    }
+  };
+  
+  for ( var i = 0; i < 5; i++ ) {
+    var position = positions[i];
+    var style = msnry.items[i].element.style;
+    for ( var prop in position ) {
+      var value = position[ prop ] + 'px';
+      var message = 'item ' + i + ' ' + prop + ' = ' + value;
+      equal( style[ prop ], value, message );
+    }
+  }
+
+});
+
+test( 'basic layout top right', function() {
+  var container = document.querySelector('#basic-layout-top-right');
+  var msnry = new Masonry( container, {
+    isOriginLeft: false,
+    columnWidth: 60
+  });
+
+  var positions = {
+    0: {
+      right: 0,
+      top: 0
+    },
+    1: {
+      right: 60,
+      top: 0
+    },
+    2: {
+      right: 120,
+      top: 0
+    },
+    3: {
+      right: 0,
+      top: 30
+    },
+    4: {
+      right: 60,
+      top: 90
+    }
+  };
+  
+  for ( var i = 0; i < 5; i++ ) {
+    var position = positions[i];
+    var style = msnry.items[i].element.style;
+    for ( var prop in position ) {
+      var value = position[ prop ] + 'px';
+      var message = 'item ' + i + ' ' + prop + ' = ' + value;
+      equal( style[ prop ], value, message );
+    }
+  }
+
+});
+
+test( 'basic layout bottom left', function() {
+  var container = document.querySelector('#basic-layout-bottom-left');
+  var msnry = new Masonry( container, {
+    isOriginTop: false,
+    columnWidth: 60
+  });
+
+  var positions = {
+    0: {
+      left: 0,
+      bottom: 0
+    },
+    1: {
+      left: 60,
+      bottom: 0
+    },
+    2: {
+      left: 120,
+      bottom: 0
+    },
+    3: {
+      left: 0,
+      bottom: 30
+    },
+    4: {
+      left: 60,
+      bottom: 90
+    }
+  };
+  
+  for ( var i = 0; i < 5; i++ ) {
+    var position = positions[i];
+    var style = msnry.items[i].element.style;
+    for ( var prop in position ) {
+      var value = position[ prop ] + 'px';
+      var message = 'item ' + i + ' ' + prop + ' = ' + value;
+      equal( style[ prop ], value, message );
+    }
+  }
+
+});
+
+test( 'basic layout bottom right', function() {
+  var container = document.querySelector('#basic-layout-bottom-right');
+  var msnry = new Masonry( container, {
+    isOriginLeft: false,
+    isOriginTop: false,
+    columnWidth: 60
+  });
+
+  var positions = {
+    0: {
+      right: 0,
+      bottom: 0
+    },
+    1: {
+      right: 60,
+      bottom: 0
+    },
+    2: {
+      right: 120,
+      bottom: 0
+    },
+    3: {
+      right: 0,
+      bottom: 30
+    },
+    4: {
+      right: 60,
+      bottom: 90
+    }
+  };
+  
+  for ( var i = 0; i < 5; i++ ) {
+    var position = positions[i];
+    var style = msnry.items[i].element.style;
+    for ( var prop in position ) {
+      var value = position[ prop ] + 'px';
+      var message = 'item ' + i + ' ' + prop + ' = ' + value;
+      equal( style[ prop ], value, message );
+    }
+  }
+
+});
+
+
+})();
