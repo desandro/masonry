b1759c9861925efacc3b0bbca2901cd0536b36ad
David DeSandro <desandrocodes@gmail.com>
2016-01-18 20:54:12 -0500

    🍹 use metafizzy/gulp-require-optimize

--- gulpfile.js
+++ gulpfile.js
@@ -39,55 +39,6 @@ gulp.task( 'hint', [ 'hint-js', 'hint-test', 'hint-task', 'jsonlint' ]);
 
 // -------------------------- RequireJS makes pkgd -------------------------- //
 
-// refactored from gulp-requirejs-optimize
-// https://www.npmjs.com/package/gulp-requirejs-optimize/
-
-var gutil = require('gulp-util');
-var through = require('through2');
-var requirejs = require('requirejs');
-var chalk = require('chalk');
-
-function rjsOptimize( options ) {
-  var stream;
-
-  requirejs.define('node/print', [], function() {
-    return function(msg) {
-      if( msg.substring(0, 5) === 'Error' ) {
-        gutil.log( chalk.red( msg ) );
-      } else {
-        gutil.log( msg );
-      }
-    };
-  });
-
-  options = options || {};
-
-  stream = through.obj(function (file, enc, cb) {
-    if ( file.isNull() ) {
-      this.push( file );
-      return cb();
-    }
-
-    options.logLevel = 2;
-
-    options.out = function( text ) {
-      var outFile = new gutil.File({
-        path: file.relative,
-        contents: new Buffer( text )
-      });
-      cb( null, outFile );
-    };
-
-    gutil.log('RequireJS optimizing');
-    requirejs.optimize( options, null, function( err ) {
-      var gulpError = new gutil.PluginError( 'requirejsOptimize', err.message );
-      stream.emit( 'error', gulpError );
-    });
-  });
-
-  return stream;
-}
-
 // regex for banner comment
 var reBannerComment = new RegExp('^\\s*(?:\\/\\*[\\s\\S]*?\\*\\/)\\s*');
 
@@ -101,6 +52,8 @@ function getBanner() {
 function addBanner( str ) {
   return replace( /^/, str );
 }
+
+var rjsOptimize = require('gulp-requirejs-optimize');
 
 gulp.task( 'requirejs', function() {
   var banner = getBanner();
